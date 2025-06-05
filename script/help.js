9c95012ad8197f0bec8f1fc1378a6b99f1971773
Gestures5 <kenshimadax@gmail.com>
2025-06-05 13:25:03 +0800

    Update help.js

--- script/help.js
+++ script/help.js
@@ -25,7 +25,7 @@ module.exports.run = async function({
       let page = 1;
       let start = (page - 1) * pages;
       let end = start + pages;
-      let helpMessage = 📃 Command List:\n\n`;
+      let helpMessage =  `📃 Command List:\n\n`;
       for (let i = start; i < Math.min(end, commands.length); i++) {
         helpMessage += `\t${i + 1}. 「 ${prefix}${commands[i]} 」\n`;
       }
