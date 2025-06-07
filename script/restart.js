2af026294f7207832a5fe71520913dcbd8854ddf
Gestures5 <kenshimadax@gmail.com>
2025-06-08 00:23:21 +0800

    Update restart.js

--- script/restart.js
+++ script/restart.js
@@ -1,11 +1,1 @@
-module.exports = {
-    description: "Restart the server",
-    role: "admin",
-    cooldown: 50,
-    execute(api, event, args, commands) {
-        api.sendMessage("Restarting the server...", event.threadID);
-        setTimeout(() => {
-            process.exit(1);
-        }, 4000);
-    }
-};
+
