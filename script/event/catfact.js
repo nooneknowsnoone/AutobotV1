f8b67c0535c2565973d040610aefe26949cf01dd
Gestures5 <kenshimadax@gmail.com>
2025-06-28 22:57:44 +0800

    Update catfact.js

--- script/event/catfact.js
+++ script/event/catfact.js
@@ -22,23 +22,14 @@ cron.schedule('0 * * * *', async () => {
     const factRes = await axios.get('https://xvi-rest-api.vercel.app/api/catfact');
     const catFact = factRes.data.fact;
 
-    const imageRes = await axios.get('https://kaiz-apis.gleeze.com/api/randomcat?limit=1&apikey=bbcc44b9-4710-41c7-8034-fa2000ea7ae5');
-    const imageUrl = imageRes.data.images[0];
-
-    const message = 😺 | CATFACTn\n\${catFact}`;
+    const message = `😺 | CATFACT\n\n${catFact}`;
 
     const api = global.api || require('somewhere').api; // Replace or inject as needed
     const threads = await api.getThreadList(25, null, ['INBOX']);
 
     for (const thread of threads) {
       if (thread.isGroup && thread.name !== thread.threadID) {
-        const imageStream = await axios({
-          method: 'get',
-          url: imageUrl,
-          responseType: 'stream',
-        });
-
-        await api.sendMessage({ body: message, attachment: imageStream.data }, thread.threadID);
+        await api.sendMessage({ body: message }, thread.threadID);
       }
     }
   } catch (err) {
