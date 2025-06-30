36d619a314a013702df409c34238382af2dddfec
Gestures5 <kenshimadax@gmail.com>
2025-07-01 02:40:10 +0800

    Update blowjob.js

--- script/blowjob.js
+++ script/blowjob.js
@@ -20,7 +20,7 @@ module.exports.run = async function ({ api, event }) {
   try {
     api.sendMessage("🔞 Fetching a Blowjob GIF...", threadID, async () => {
       try {
-        const res = await axios.get("https://kaiz-apis.gleeze.com/api/gpt-4.1", {
+        const res = await axios.get("https://kaiz-apis.gleeze.com/api/bluearchive", {
           params: {
             ask: "Send a random blowjob GIF",
             uid: senderID,
