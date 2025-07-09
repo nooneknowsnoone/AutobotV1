22093785ef2d7e22d2f4c7154cbc26f37402f943
Gestures5 <kenshimadax@gmail.com>
2025-07-09 17:04:51 +0800

    Update auto.js

--- auto.js
+++ auto.js
@@ -119,6 +119,9 @@ const routes = [{
 }, {
   path: '/online_user',
   file: 'online.html'
+  }, (
+path: '/automated_fba',
+  file: 'autobot.html'
 }, ];
 routes.forEach(route => {
   app.get(route.path, (req, res) => {
