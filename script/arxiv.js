9497b0b0b7853575cb6a7c35e87f91ffb9147066
Gestures5 <kenshimadax@gmail.com>
2025-07-11 16:03:12 +0800

    Update arxiv.js

--- script/arxiv.js
+++ script/arxiv.js
@@ -4,7 +4,7 @@ module.exports.config = {
   name: 'arxiv',
   version: '1.0.0',
   role: 0,
-  hasPrefix: false,
+  hasPrefix: true,
   aliases: [],
   description: 'Fetch article from arXiv',
   usage: 'arxiv <word>',
