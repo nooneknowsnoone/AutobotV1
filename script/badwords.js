4d90fc928a82f258b29ab6cbf26e5c5cadf33e5b
Gestures5 <kenshimadax@gmail.com>
2025-07-11 16:04:06 +0800

    Update badwords.js

--- script/badwords.js
+++ script/badwords.js
@@ -32,7 +32,7 @@ module.exports.config = {
     role: 2,
     credits: "Jonell",
     description: "Manage and enforce banned words",
-    hasPrefix: false,
+    hasPrefix: true,
     usages: "add [word] | remove [word] | list | on | off",
     cooldown: 5,
 };
