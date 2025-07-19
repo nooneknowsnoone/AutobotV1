810d5f51bfcd600b2369b18bd3c7ad0ac229f9d8
Gestures5 <kenshimadax@gmail.com>
2025-07-19 19:27:26 +0800

    Create hi.js

---
+++ script/hi.js
@@ -0,0 +1,37 @@
+const axios = require("axios");
+
+let fontEnabled = true;
+
+function formatFont(text) {
+  const fontMapping = {
+    a: "𝖺", b: "𝖻", c: "𝖼", d: "𝖽", e: "𝖾", f: "𝖿", g: "𝗀", h: "𝗁", i: "𝗂", j: "𝗃", k: "𝗄", l: "𝗅", m: "𝗆",
+    n: "𝗇", o: "𝗈", p: "𝗉", q: "𝗊", r: "𝗋", s: "𝗌", t: "𝗍", u: "𝗎", v: "𝗏", w: "𝗐", x: "𝗑", y: "𝗒", z: "𝗓",
+    A: "𝖠", B: "𝖡", C: "𝖢", D: "𝖣", E: "𝖤", F: "𝖥", G: "𝖦", H: "𝖧", I: "𝖨", J: "𝖩", K: "𝖪", L: "𝖫", M: "𝖬",
+    N: "𝖭", O: "𝖮", P: "𝖯", Q: "𝖰", R: "𝖱", S: "𝖲", T: "𝖳", U: "𝖴", V: "𝖵", W: "𝖶", X: "𝖷", Y: "𝖸", Z: "𝖹"
+  };
+
+  let formattedText = "";
+  for (const char of text) {
+    formattedText += fontEnabled && fontMapping[char] ? fontMapping[char] : char;
+  }
+  return formattedText;
+}
+
+module.exports.config = {
+  name: "hi",
+  version: "1.0.0",
+  role: 0,
+  hasPrefix: false,
+  aliases: ["hello", "yo", "yow", "bot", "hey" ],
+  description: "greetings.",
+  usage: "hi",
+  credits: "Chatbot system",
+  cooldown: 2,
+};
+
+module.exports.run = async function ({ api, event }) {
+  const threadID = event.threadID;
+  const messageID = event.messageID;
+
+  return api.sendMessage(formatFont("Hello 😊 How can i assist you today?\n\nKindly simplify type “help” to see my available commands "), threadID, messageID);
+};
