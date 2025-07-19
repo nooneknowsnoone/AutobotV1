ffe288490deb0ccc5edcd6a01fbb1b207256d3e9
Gestures5 <kenshimadax@gmail.com>
2025-07-19 22:21:43 +0800

    Update hi.js

--- script/hi.js
+++ script/hi.js
@@ -17,7 +17,7 @@ module.exports.run = async function ({ api, event }) {
   const messageID = event.messageID;
 
   return api.sendMessage(
-    "𝖧𝖾𝗅𝗅𝗈 😊 𝖧𝗈𝗐 𝖼𝖺𝗇 𝗂 𝖺𝗌𝗌𝗂𝗌𝗍 𝗒𝗈𝗎 𝗍𝗈𝖽𝖺𝗒?\n\n𝖪𝗂𝗇𝖽𝗅𝗒 𝗌𝗂𝗆𝗉𝗅𝗂𝖿𝗒 𝗍𝗒𝗉𝖾 “𝗁𝖾𝗅𝗉” 𝗍𝗈 𝗌𝖾𝖾 𝗆𝗒 𝖺𝗏𝖺𝗂𝗅𝖺𝖻𝗅𝖾 𝖼𝗈𝗆𝗆𝖺𝗇𝖽𝗌.\n\n𝖨𝖿 𝗒𝗈𝗎 𝗇𝖾𝖾𝖽 24/7 𝗌𝖾𝗋𝗏𝗂𝖼𝖾𝗌 𝖿𝗈𝗅𝗅𝗈𝗐 𝗈𝗎𝗋 𝗆𝖺𝗂𝗇 𝗉𝖺𝗀𝖾𝖻𝗈𝗍:, follow our main pagebot:\n\n🔗: fb.com/hersheyassistant",
+    "𝖧𝖾𝗅𝗅𝗈 😊 𝖧𝗈𝗐 𝖼𝖺𝗇 𝗂 𝖺𝗌𝗌𝗂𝗌𝗍 𝗒𝗈𝗎 𝗍𝗈𝖽𝖺𝗒?\n\n𝖪𝗂𝗇𝖽𝗅𝗒 𝗌𝗂𝗆𝗉𝗅𝗂𝖿𝗒 𝗍𝗒𝗉𝖾 “𝗁𝖾𝗅𝗉” 𝗍𝗈 𝗌𝖾𝖾 𝗆𝗒 𝖺𝗏𝖺𝗂𝗅𝖺𝖻𝗅𝖾 𝖼𝗈𝗆𝗆𝖺𝗇𝖽𝗌.\n\n𝖨𝖿 𝗒𝗈𝗎 𝗇𝖾𝖾𝖽 24/7 𝗌𝖾𝗋𝗏𝗂𝖼𝖾𝗌 𝖿𝗈𝗅𝗅𝗈𝗐 𝗈𝗎𝗋 𝗆𝖺𝗂𝗇 𝗉𝖺𝗀𝖾𝖻𝗈𝗍:\n\n🔗: fb.com/hersheyassistant",
     threadID,
     messageID
   );
